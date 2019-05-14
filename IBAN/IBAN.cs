99e15f6ec7af0fb0ace3e6c3a850010d03597e17
DANI3L11 <garciaantoniodaniel11@gmail.com>
2019-05-14 17:34:03 +0200

    Primera codificación del proyecto

---
+++ IBAN/IBAN.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IBAN
+{
+    public class IBANP
+    {
+        // Variables
+        private string iban;
+        private string E = "14";
+        private string S = "28";
+
+        // Propiedades
+        public string Iban
+        {
+            get { return iban; }
+            set { iban = value; }
+        }
+
+        // Constructores
+        public IBANP() { }
+
+        // Métodos
+        public string CrearIban(string cuenta)
+        {
+            // Paso preliminar
+            // Paso 1
+            string ibanTmp = cuenta + "ES00";
+            
+            // Paso 2
+            ibanTmp = ibanTmp.Substring(19, 2);
+            ibanTmp = ibanTmp.Insert(19, E);
+            ibanTmp = ibanTmp.Insert(21, S);
+
+            return ibanTmp;
+        }
+
+    }
+}
---
+++ IBAN_TEST/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using IBAN;
+
+
+namespace IBAN_TEST
+{
+    [TestFixture]
+    public class IBAN_TEST
+    {
+        [Test]
+        public void CrearIban()
+        {
+            string cuenta = "00120345030000067890";
+            IBANP iban = new IBANP();
+            string resultado = iban.CrearIban(cuenta);
+            Assert.AreEqual("00120345030000067890142800", resultado);
+        }
+    }
+}
